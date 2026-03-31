3755b31090e9f68520cc076b741e111f5531d3a1
Zeki Bıyıklı <zekeriya.biyikli@monodijital.com>
2026-03-31 20:09:58 +0300

    FoursquareClone (Parse)

---
+++ FoursquareClone/FoursquareClone/PlaceModel.swift
@@ -0,0 +1,25 @@
+//
+//  PlaceModel.swift
+//  FoursquareClone
+//
+//  Created by Atil Samancioglu on 9.08.2019.
+//  Copyright © 2019 Atil Samancioglu. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PlaceModel {
+    
+    static let sharedInstance = PlaceModel()
+    
+    var placeName = ""
+    var placeType = ""
+    var placeAtmosphere = ""
+    var placeImage = UIImage()
+    var placeLatitude = ""
+    var placeLongitude = ""
+    
+    private init(){}
+    
+}
